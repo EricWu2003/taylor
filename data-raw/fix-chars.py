4bc852b2b15e0a55378cf1572d4a2fb7a3ece122
Eric Wu <eric.dianhao.wu@gmail.com>
2022-10-14 14:59:04 -0700

    enforce stricter ascii chars for lyrics

--- data-raw/fix-chars.py
+++ data-raw/fix-chars.py
@@ -3,28 +3,28 @@ from os.path import join
 from os import path
 
 def replaceChars(filepath):
-    lyrics = ""
-    with open(filepath, 'r') as f:
-        lyrics = f.read()
+	lyrics = ""
+	with open(filepath, 'r') as f:
+		lyrics = f.read()
 
-        lyrics = lyrics.replace("е", "e")
-        # lyrics = lyrics.replace("”", "\"")
-        # lyrics = lyrics.replace("“", "\"")
-        # lyrics = lyrics.replace("’", "\'")
-        lyrics = lyrics.replace("\u2005", " ")
-        lyrics = lyrics.replace("\u205f", " ")
-        lyrics = lyrics.replace("\u200b", " ")
-        lyrics = lyrics.replace("—", "-") # this is a long dash
-        lyrics = lyrics.replace("–", "-") # this is a different long dash
-        # lyrics = lyrics.replace("…", "...")
+		lyrics = lyrics.replace("е", "e")
+		lyrics = lyrics.replace("”", "\"")
+		lyrics = lyrics.replace("“", "\"")
+		lyrics = lyrics.replace("’", "\'")
+		lyrics = lyrics.replace("\u2005", " ")
+		lyrics = lyrics.replace("\u205f", " ")
+		lyrics = lyrics.replace("\u200b", " ")
+		lyrics = lyrics.replace("—", "-") # this is a long dash
+		lyrics = lyrics.replace("–", "-") # this is a different long dash
+		lyrics = lyrics.replace("…", "...")
 
-    with open(filepath, 'w') as f:
-        f.write(lyrics)
+	with open(filepath, 'w') as f:
+		f.write(lyrics)
 
 
 
 working_dir = path.dirname(__file__)
-raw_lyric_dir = "data-raw/lyrics"
+raw_lyric_dir = "lyrics"
 
 os.chdir(raw_lyric_dir)
 album_titles = [name for name in os.listdir(".") if os.path.isdir(name)]
@@ -33,6 +33,7 @@ album_titles = [name for name in os.listdir(".") if os.path.isdir(name)]
 for album in album_titles:
 	os.chdir(join(working_dir, raw_lyric_dir, album))
 	for path in os.listdir("."):
+		print(album, path)
 		if not os.path.isfile(path):
 			continue
 		replaceChars(path)
